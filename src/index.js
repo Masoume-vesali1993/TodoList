e7ed3df6764f899b090db04527eb18fb693ccaf7
masoume <masoumevsl198@gmail.com>
2020-03-26 01:41:08 +0430

    show page

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+var destination = document.querySelector("#container");
+
+ReactDOM.render(
+    <div>
+        <p>Hello!</p>
+    </div>,
+    destination
+
+);
