b528bf78c105660527ff3b2cb3b98856f72e89fc
masoume <masoumevsl198@gmail.com>
2020-03-27 02:20:42 +0430

    new file in part src

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import TodoList from "./TodoItems";
 
 class TodoList extends Component {
     constructor(props) {
@@ -45,6 +46,7 @@ class TodoList extends Component {
                         <button type="submit">add</button>
                     </form>
                 </div>
+                <TodoList entries={this.state.items}/>
             </div>
         );
     }
